d1d1302f4afb7dc461682f492fd7a4d700beaad5
Alexander Friedl <alexander.friedl@mytum.de>
2017-08-19 18:03:40 +0200

    Adapted server logic to newer Mongoose API

--- server/js/database.js
+++ server/js/database.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = class Database {
-  constructor () {
+  constructor() {
     this.debug = require('debug')('push:database')
 
     this.mongoose = require('mongoose')
@@ -11,16 +11,19 @@ module.exports = class Database {
     db.on('error', (err) => this.debug('database connection error:', err))
     db.once('open', () => this.debug('Opened connection to MongoDB'))
 
-    this.DeviceToken = this.mongoose.model('DeviceToken', { token: String })
+    const tokenSchema = this.mongoose.Schema({
+      token: String
+    });
+    this.DeviceToken = this.mongoose.model('DeviceToken', tokenSchema)
   }
 
-  saveDeviceToken (token) {
+  saveDeviceToken(token) {
     this.debug('Trying to store new DeviceToken', token)
-    const t = new this.DeviceToken(token)
-    return t.save(token)
+    const t = new this.DeviceToken({ token: token })
+    return t.save()
   }
 
-  async fetchDeviceTokens () {
+  async fetchDeviceTokens() {
     const tokens = await this.DeviceToken.find().select('token').exec()
     return tokens.map(d => d.token)
   }
