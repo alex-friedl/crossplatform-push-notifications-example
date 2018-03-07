90f59be591b2ac497e9f9da96fd12a127927db6e
Alexander Friedl <alexander.friedl@mytum.de>
2018-03-07 21:44:15 +0100

    Add router.allowedMethods middleware

--- server/js/server.js
+++ server/js/server.js
@@ -16,6 +16,7 @@ app.use(bodyParser({
   enableTypes: ['text'],
 }));
 app.use(router.routes());
+app.use(router.allowedMethods());
 
 router.post('/token', async (ctx) => {
   const token = ctx.request.body;
